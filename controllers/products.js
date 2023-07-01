0f17e4a7171e6ed324948736347f296adee3184f
Gurudatta <gurudatta1455@gmail.com>
2023-07-02 01:58:45 +0530

    Data Update

--- controllers/products.js
+++ controllers/products.js
@@ -22,11 +22,11 @@ const getAllProducts = async (req, res) => {
     apiData = apiData.select(selectFix);
   }
 
-  let page = Number(req.query.page) || 1;
-  let limit = Number(req.query.limit) || 5;
-  let skip = (page - 1) * limit;
+  // let page = Number(req.query.page) || 1;
+  // let limit = Number(req.query.limit) || 5;
+  // let skip = (page - 1) * limit;
 
-  apiData = apiData.skip(skip).limit(limit);
+  // apiData = apiData.skip(skip).limit(limit);
 
   console.log(queryObject);
 

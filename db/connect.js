148a9685bb2db98a00d7bab560ce65593a811a2b
Gurudatta <gurudatta1455@gmail.com>
2023-07-10 13:31:19 +0530

    Packege

--- db/connect.js
+++ db/connect.js
@@ -5,7 +5,7 @@ const connectDb = async (uri) => {
     await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    });
+    }); 
     console.log("Connected successfully to the database");
   } catch (err) {
     console.error("Failed to connect to the database:", err);
